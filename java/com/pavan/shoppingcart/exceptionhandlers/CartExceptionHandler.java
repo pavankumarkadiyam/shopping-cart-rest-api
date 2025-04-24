0d18f36313b8efcfd7cba0d2afc1e23a2584a2ad
pavankumarkadiyam <110335367+pavankumarkadiyam@users.noreply.github.com>
2025-04-25 01:32:47 +0530

    Delete java/com/pavan/shoppingcart directory

--- java/com/pavan/shoppingcart/exceptionhandlers/CartExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.pavan.shoppingcart.exceptionhandlers;
-
-
-@SuppressWarnings("serial")
-public class CartExceptionHandler extends RuntimeException {
-	public CartExceptionHandler(String message) {
-		super(message);
-	}
-}
